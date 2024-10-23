01b8bc40a02ca7cd627d2435d22fac45cb820881
danirisdiandita <normadani2016@gmail.com>
2024-10-23 13:02:51 +0700

    first commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MODULE_NAME=cpp/adder
+OUTPUT_JS=src/wasm/adder_wasm.js
+
+mkdir -p src/wasm
+
+emcc ${MODULE_NAME}.cpp \
+ -o ${OUTPUT_JS} \
+ -s EXPORT_ES6=1 \
+ -s 'EXPORT_NAME="$MODULE_NAME"' \
+ -s 'ENVIRONMENT="web"'
